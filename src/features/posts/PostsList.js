232b1c4988099432d0d8250751bdcb1c2c4431d4
ivicakartelo <ivicakartelo@users.noreply.github.com>
2022-11-01 21:30:55 +0100

    EditPostForm

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+export const PostsList = () => {
+  const posts = useSelector(state => state.posts)
+
+  const renderedPosts = posts.map(post => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`}>
+        View post
+      </Link>
+    </article>
+  ))
+
+return (
+  <section>
+    <h2>Posts</h2>
+    {renderedPosts}    
+  </section>
+)
+}
